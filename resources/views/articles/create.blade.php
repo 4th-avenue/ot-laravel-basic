bc251017605e577cbf142c78b0ec825f3a902381
mystee <quebec706@gmail.com>
2023-07-03 18:03:25 +0900

    간단한 글쓰기 화면 만들기

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>글쓰기</h1>
+    <from action="/articles" method="POST">
+        <input type="text">
+        <input type="button" value="저장하기">
+    </form>
+</body>
+</html>
