fc3cd5febf4c1c5a764752c302fe11aec3328e56
mystee <quebec706@gmail.com>
2023-07-03 20:44:27 +0900

    TailwindCSS로 꾸미기

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,10 +7,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>글쓰기</h1>
-    <from action="/articles" method="POST">
-        <input type="text">
-        <input type="button" value="저장하기">
-    </form>
+    <div class="container p-5">
+        <h1 class="text-2xl">글쓰기</h1>
+        <from action="/articles" method="POST">
+            <input type="text" class="block w-full mt-3 mb-2 rounded">
+            <input type="button" value="저장하기" class="py-1 px-3 bg-black text-white rounded text-xs">
+        </form>
+    </div>
 </body>
 </html>
