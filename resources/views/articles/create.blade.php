e2cd9d807a25a063dce6885f4b22fce3528e0595
mystee <quebec706@gmail.com>
2023-07-03 22:48:07 +0900

    글 저장하기 라우트 추가하기 (CSRF)

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,6 +10,7 @@
     <div class="container p-5">
         <h1 class="text-2xl">글쓰기</h1>
         <form action="/articles" method="POST">
+            @csrf
             <input type="text" class="block w-full mt-3 mb-2 rounded">
             <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
         </form>
