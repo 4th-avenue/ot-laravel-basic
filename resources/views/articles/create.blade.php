561ca7ab6f64c013659788113a49f4248225cf16
mystee <quebec706@gmail.com>
2023-07-03 22:38:23 +0900

    오타랑 button 태그 수정

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -9,9 +9,9 @@
 <body>
     <div class="container p-5">
         <h1 class="text-2xl">글쓰기</h1>
-        <from action="/articles" method="POST">
+        <form action="/articles" method="POST">
             <input type="text" class="block w-full mt-3 mb-2 rounded">
-            <input type="button" value="저장하기" class="py-1 px-3 bg-black text-white rounded text-xs">
+            <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
         </form>
     </div>
 </body>
