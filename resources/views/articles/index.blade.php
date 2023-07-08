5aeb1da685ddf92177504a38a99099201213ab5c
mystee <quebec706@gmail.com>
2023-07-08 14:41:59 +0900

    CRUD 기본 라우트 7개를 리소스 라우트 한 줄로 대체함

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -16,7 +16,7 @@
                 <p>{{$article->created_at->diffForHumans()}}</p>
                 <div class="flex flex-row mt-2">
                     <p class="mr-1"><a href="{{route('articles.edit', ['article' => $article->id])}}" class="button rounded border bg-blue-500 px-2 py-1 text-xs text-white">수정</a></p>
-                    <form action="{{route('articles.delete', ['article' => $article])}}" method="POST">
+                    <form action="{{route('articles.destroy', ['article' => $article])}}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button class="py-1 px-3 bg-red-500 text-white rounded text-xs">삭제</button>
