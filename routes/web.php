e35c75243414dbba09df6a3c089a166d23ce0262
mystee <quebec706@gmail.com>
2023-07-13 13:58:47 +0900

    username 수정 기능 추가

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,4 @@ Route::resource('articles', ArticleController::class);
 Route::resource('comments', CommentController::class);
 
 Route::get('profile/{user:username}', [ProfileController::class, 'show'])->name('profile')
-->where('user', '[A-Za-z0-9-]+');
+->where('user', '^[A-Za-z0-9-]+$');
